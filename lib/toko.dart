bbb58b24378c41de064e259234d6d6235b65d0ef
fransisGil <fransiskusgilberthojunjhiek@gmail.com>
2026-05-28 10:10:13 +0700

    starting checkout

--- lib/toko.dart
+++ lib/toko.dart
@@ -19,16 +19,40 @@ class _TokoScreenState extends State<TokoScreen> {
 
   @override
   void initState() {
-    _getDataKategori();
     _getDataProduk();
+    _getDataKategori();
     super.initState();
+  }
+
+  void _getDataKategori() async {
+    try {
+      final data = await AppConfig().database.listDocuments(
+            databaseId: AppConfig().databaseID,
+            collectionId: 'kategori',
+          );
+
+      List<Kategori> dataKategori = [];
+      for (var element in data.documents) {
+        dataKategori.add(Kategori(
+          id: element.$id,
+          nama: element.data['nama'],
+        ));
+      }
+      dataKategori.add(Kategori(id: '', nama: 'Semua Kategori'));
+      setState(() {
+        _dataKategori = dataKategori;
+      });
+    } on AppwriteException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error Get Data Kategori : $e')));
+    }
   }
 
   void _getDataProduk() async {
     try {
       final data = await AppConfig().database.listDocuments(
             databaseId: AppConfig().databaseID,
-            collectionId: 'Product',
+            collectionId: 'produk',
           );
 
       List<Produk> dataProduk = [];
@@ -54,86 +78,59 @@ class _TokoScreenState extends State<TokoScreen> {
     }
   }
 
-  void _getDataKategori() async {
-    try {
-      final data = await AppConfig().database.listDocuments(
-            databaseId: AppConfig().databaseID,
-            collectionId: 'category',
-          );
-
-      List<Kategori> dataKategori = [];
-      for (var element in data.documents) {
-        dataKategori.add(Kategori(
-          id: element.$id,
-          nama: element.data['nama'],
-        ));
-      }
-      dataKategori.add(Kategori(id: '', nama: "Semua kategori"));
-      setState(() {
-        _dataKategori = dataKategori;
-      });
-    } on AppwriteException catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Error Get Data Kategori : $e')));
-    }
-  }
-
   Widget daftarProduk() {
     return Column(
       spacing: 12,
       children: [
         DropdownMenu(
-          width: MediaQuery.sizeOf(context).width,
-            dropdownMenuEntries: _dataKategori
-                .map(
-                  (e) => DropdownMenuEntry(value: e.id, label: e.nama.replaceRange(0, 1, e.nama[0].toUpperCase())),
-                )
-                .toList()),
+          label: Text('Filter Kategori'),
+          width: double.infinity,
+          dropdownMenuEntries: _dataKategori
+              .map(
+                (e) => DropdownMenuEntry(value: e.id, label: e.nama),
+              )
+              .toList(),
+        ),
         Expanded(
           child: GridView.builder(
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
-              childAspectRatio: 0.6,
+              childAspectRatio: 0.55,
             ),
             itemCount: _dataProduk.length,
             itemBuilder: (context, index) {
               var produk = _dataProduk[index];
-          
+
               return Card(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: GridTile(
                     header: Image.network(produk.fotoUrl),
                     footer: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(produk.nama),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('Harga'),
-                            Text('Stok'),
-                          ],
+                        Text(produk.harga.toString()),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              if (_dataKeranjang.any(
+                                (element) => element.produk.id == produk.id,
+                              )) {
+                                int index = _dataKeranjang.indexWhere(
+                                  (element) => element.produk.id == produk.id,
+                                );
+                                _dataKeranjang[index].tambahQty();
+                              } else {
+                                _dataKeranjang.add(Keranjang(produk: produk));
+                              }
+                            });
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Produk ditambahkan')),
+                            );
+                          },
+                          child: Text('Add to Cart'),
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(produk.harga.toString()),
-                            Text(produk.stok.toString()),
-                          ],
-                        ),
-                        ElevatedButton(onPressed: (){
-                          setState(() {
-                            if (_dataKeranjang.any((element) => element.produk.id == produk.id,)) {
-                              int index = _dataKeranjang.indexWhere((element) => element.produk.id == produk.id,);
-                              _dataKeranjang[index].tambahQty();
-                            } else {
-                              _dataKeranjang.add(Keranjang(produk: produk));
-                            }
-                          });
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('Produk ditambah'))
-                          );
-                        }, child: Icon(Icons.add_shopping_cart))
                       ],
                     ),
                     child: Text(''),
@@ -148,7 +145,131 @@ class _TokoScreenState extends State<TokoScreen> {
   }
 
   Widget daftarKeranjang() {
-    return Text('Daftar Keranjang');
+    return Column(
+      spacing: 12,
+      children: [
+        Text(
+          'Keranjang Belanja',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: TextButton(
+            onPressed: () {
+              //koding hapus semua produk
+              setState(() {});
+              _dataKeranjang.clear();
+              _indexMenu = 0;
+            },
+            child: Text('Hapus Semua'),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _dataKeranjang.length,
+            itemBuilder: (context, index) {
+              var itemCart = _dataKeranjang[index];
+
+              return Card(
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: Colors.blue,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Image.network(itemCart.produk.fotoUrl),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 12,
+                    children: [
+                      Text(itemCart.produk.nama),
+                      Text('${itemCart.jumlah} x ${itemCart.produk.harga}'),
+                      Text(itemCart.subTotal.toString()),
+                    ],
+                  ),
+                  subtitle: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          //koding kurang qty produk
+                          setState(() {
+                            _dataKeranjang[index].kurangQty();
+                          });
+                        },
+                        icon: Icon(Icons.remove_circle_outline),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          //koding tambah qty produk
+                          setState(() {
+                            _dataKeranjang[index].tambahQty();
+                          });
+                        },
+                        icon: Icon(Icons.add_circle_outline),
+                      ),
+                      Spacer(),
+                      TextButton(
+                        onPressed: () {
+                          //koding hapus produk
+                          _dataKeranjang.removeAt(index);
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text('Produk berhasil dihapus.')));
+                        },
+                        style: TextButton.styleFrom(
+                          foregroundColor: Colors.red,
+                        ),
+                        child: Text('Hapus Produk'),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Text('Total Keranjang'),
+                Text(
+                  getTotalKeranjang(),
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                //koding check out
+              },
+              label: Text('Check Out'),
+              icon: Icon(
+                Icons.arrow_forward,
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
+  String getTotalKeranjang() {
+    //koding get total keranjang
+    double sum = 0;
+    for (Keranjang keranjang in _dataKeranjang) {
+      sum += keranjang.subTotal;
+    }
+    return '$sum';
   }
 
   @override
@@ -208,5 +329,5 @@ class Keranjang {
 
   void tambahQty() => jumlah++;
   void kurangQty() => jumlah > 1 ? jumlah-- : jumlah = 1;
-  get subTotal => produk.harga * jumlah;
+  double get subTotal => produk.harga * jumlah;
 }
